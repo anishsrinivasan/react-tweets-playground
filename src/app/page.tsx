425ac80458d720aa5ed70d02dbaea83f222742a3
Anish <itsanishsrinivasan@gmail.com>
2023-07-27 04:40:34 +0530

    improve : og-image & ui fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home({
 }: {
   searchParams: { tweetId: string };
 }) {
-  const tweetId = searchParams?.tweetId || "1682978324375543808";
+  const tweetId = searchParams?.tweetId || "1683920951807971329";
 
   return (
     <main
@@ -16,13 +16,17 @@ export default function Home({
       className="container flex min-h-screen flex-col items-center justify-between p-16 w-full"
     >
       <div>
-        <div className="mb-[20px]">
+        <div className="mb-[10px]">
           <h1 className="text-[30px] md:text-[36px] font-medium text-center mb-[6px] text-[#0d0b0b]">
             Tweet / Xeet
           </h1>
-          <p className="text-[#0d0b0b]">
+          <p className="text-[#0d0b0b] text-[14px]">
             This playground is used to display the embedded tweets or xeets
-            using <strong>react-tweets</strong>.
+            using{" "}
+            <a href="https://react-tweet.vercel.app/" target="_blank">
+              <strong>react-tweets</strong>
+            </a>
+            .
           </p>
         </div>
         <div className="flex items-center justify-between lg:min-w-[600px] mb-[10px]">
@@ -39,8 +43,8 @@ export default function Home({
           <ThemeSwitch />
         </div>
       </div>
-      <div>
-        <p className="text-center">
+      <div className="mt-[20px]">
+        <p className="text-center text-[14px]">
           Built using NextJS, Tailwind, UI Components on Vercel.
         </p>
       </div>
